79773ff642ff10e952cc4ac16a16dc41d008f130
Mitsuaki Wada <49638956+mi-wada@users.noreply.github.com>
2025-11-27 11:30:04 +0900

    Stop ALB (#8)

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
-output "alb_dns_name" {
-  value       = aws_lb.ui.dns_name
-  description = "ALB DNS name"
-}
+# output "alb_dns_name" {
+#   value       = aws_lb.ui.dns_name
+#   description = "ALB DNS name"
+# }
 
 output "github_actions_role_arn" {
   value       = aws_iam_role.github_actions.arn
